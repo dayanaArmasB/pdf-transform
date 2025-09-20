e2f7c0eaa88d55ee102269e0334c8e0bfcc2b260
dayanaArmasB <dyarmasb@hotmail.com>
2025-09-19 20:49:49 -0500

    arreglos en las rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,28 @@ import { ModelosDocumentosComponent } from './Components/modelos-documentos/mode
 import { DocumentoComponent } from './Components/documento/documento.component';
 
 export const routes: Routes = [
-  { path: 'modelos', component: ModelosDocumentosComponent },
+  { 
+    path: 'login', 
+    loadComponent: () => import('./auth/login/login.component').then(m => m.LoginComponent),
+
+  },
+  { 
+    path: 'register', 
+    loadComponent: () => import('./auth/register/register.component').then(m => m.RegisterComponent),
+
+  },
+  { 
+    path: 'recover', 
+    loadComponent: () => import('./auth/recover/recover.component').then(m => m.RecoverComponent),
+ },
+  { 
+    path: 'reset-password', 
+    loadComponent: () => import('./auth/reset-password/reset-password.component').then(m => m.ResetPasswordComponent),
+  
+  },
   { path: 'nosotros', component: NosotrosComponent },
-  { path: 'contrato', component: DocumentoComponent },
-  { path: '', redirectTo: 'nosotros', pathMatch: 'full' }, // Ruta por defecto
-  { path: '**', redirectTo: 'nosotros' } // Ruta para no encontrados
+  { path: 'documento', component: DocumentoComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' }, // Ruta por defecto
+  { path: '**', redirectTo: '' },
 
 ];
